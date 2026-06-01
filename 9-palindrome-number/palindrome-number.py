b4f1e40a11cec2c3f827a882de79f0749eb05c66
MIRIMIDI CHARANI <charinimirimidi006@gmail.com>
2026-06-01 19:18:21 +0530

    Time: 6 ms (67.86%) | Memory: 19.2 MB (53.75%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,11 +1,8 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
         if x<0:
             return False
         x_str = str(x)
         return x_str == x_str[::-1]
 
+        
