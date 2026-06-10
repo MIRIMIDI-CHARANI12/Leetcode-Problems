ddc3f63a21d1d12568ae562670950f48a6ea0eb4
MIRIMIDI CHARANI <charinimirimidi006@gmail.com>
2026-06-10 22:42:31 +0530

    Time: 15 ms (71.04%) | Memory: 21.3 MB (77.18%) - LeetSync

--- 135-candy/candy.py
+++ 135-candy/candy.py
@@ -8,4 +8,6 @@ class Solution:
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
                 candies[i]=max(candies[i],candies[i+1]+1)
-        return sum(candies)                        
+        return sum(candies)     
+        
+                           
