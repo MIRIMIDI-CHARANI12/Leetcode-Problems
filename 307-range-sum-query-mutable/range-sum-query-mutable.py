444d33142e41ee2360a013d7e34178b60416134b
MIRIMIDI CHARANI <charinimirimidi006@gmail.com>
2026-09-14 23:05:21 +0530

    Time: 380 ms (71.94%) | Memory: 39.1 MB (51.82%) - LeetSync

---
+++ 307-range-sum-query-mutable/range-sum-query-mutable.py
@@ -0,0 +1,29 @@
+class NumArray:
+
+    def __init__(self, nums):
+        self.n = len(nums)
+        self.nums = nums
+        self.tree = [0] * (self.n + 1)
+
+        for i in range(self.n):
+            self.add(i + 1, nums[i])
+
+    def add(self, i, val):
+        while i <= self.n:
+            self.tree[i] += val
+            i += i & -i
+
+    def update(self, index, val):
+        diff = val - self.nums[index]
+        self.nums[index] = val
+        self.add(index + 1, diff)
+
+    def prefixSum(self, i):
+        total = 0
+        while i > 0:
+            total += self.tree[i]
+            i -= i & -i
+        return total
+
+    def sumRange(self, left, right):
+        return self.prefixSum(right + 1) - self.prefixSum(left)
