e059884f1c51e4b3c3b8c7cf0345a7d07ad2d681
MIRIMIDI CHARANI <charinimirimidi006@gmail.com>
2026-09-16 22:42:31 +0530

    Time: 630 ms (19.34%) | Memory: 24.6 MB (58.89%) - LeetSync

---
+++ 816-design-hashset/design-hashset.py
@@ -0,0 +1,15 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.s = []
+
+    def add(self, key: int) -> None:
+        if key not in self.s:
+            self.s.append(key)
+
+    def remove(self, key: int) -> None:
+        if key in self.s:
+            self.s.remove(key)
+
+    def contains(self, key: int) -> bool:
+        return key in self.s
