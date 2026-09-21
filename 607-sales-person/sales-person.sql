a9451ec0d5af400257b26685003b25580881ac98
MIRIMIDI CHARANI <charinimirimidi006@gmail.com>
2026-09-21 20:46:54 +0530

    Time: 1507 ms (70.90%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,10 +1,10 @@
 # Write your MySQL query statement below
 SELECT s.name
 FROM SalesPerson s
-WHERE s.sales_id NOT IN (
-    SELECT o.sales_id
-    FROM Orders o
-    JOIN Company c
-        ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-);
+left join Orders as o 
+on s.sales_id=o.sales_id
+left join Company as c
+on o.com_id=c.com_id
+group by s.name
+having sum(c.name="RED")=0
+or sum(c.name="RED") is null;
